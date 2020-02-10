c7032bc33093c43cad481e9c5395c2275274971c
Capriskrald <hansenmann2497@hotmail.com>
2020-02-10 09:36:06 +0100

    Added top 10 baby names functionality

--- BabyNames/MainWindow.xaml.cs
+++ BabyNames/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
@@ -25,29 +26,72 @@ namespace BabyNames
         public MainWindow()
         {
             InitializeComponent();
+            babyNames = new List<BabyName>();
             decadeTopNames = new ObservableCollection<string>();
             ListDecadeTopNames.ItemsSource = decadeTopNames;
         }
 
         private ObservableCollection<string> decadeTopNames;
         private List<BabyName> babyNames;
+        private string[,] rankMatrix = new string[11, 10];
 
         private void ListDecadeTopNames_Loaded(object sender, RoutedEventArgs e)
         {
+            bool fileFound = true;
             string filePath = @".\babynames.txt";
             FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
             StreamReader sr = new StreamReader(fs, Encoding.Default);
-            
-            string str;
-            List<string> lines = new List<string>();
-            int index = 0;
 
-            //while (!sr.EndOfStream)
-            while(index < 10)
+            BabyName babyName;
+
+            try
             {
-                str = sr.ReadLine();
-                decadeTopNames.Add(str);
-                index++;
+                while (!sr.EndOfStream)
+                {
+                    string str = sr.ReadLine();
+                    decadeTopNames.Add(str);
+                    babyName = new BabyName(str);
+                    babyNames.Add(babyName);
+                }
+            }
+
+            catch
+            {
+                MessageBox.Show("Error loading baby names");
+                fileFound = false;
+            }
+
+            if(fileFound)
+            {
+                foreach (BabyName name in babyNames)
+                {
+                    for (int decade = 1900; decade <= 2000; decade += 10)
+                    {
+                        int rank = name.Rank(decade);
+                        if (rank > 0 && rank < 11)
+                        {
+                            int decadeIndex = (decade - 1900) / 10;
+                            if (rankMatrix[decadeIndex, rank - 1] == null)
+                                rankMatrix[decadeIndex, rank - 1] = name.Name;
+                            else
+                                rankMatrix[decadeIndex, rank - 1] += " and " + name.Name;
+                        }
+                    }
+                }
+            }
+            
+        }
+
+        private void ListDecades_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListBoxItem lbi = ListDecades.SelectedItem as ListBoxItem;
+            int decade = Convert.ToInt32(lbi.Content);
+            int decadeIndex = (decade - 1900) / 10;
+            decadeTopNames.Clear();
+
+            for (int i = 1; i < 11; i++)
+            {
+                decadeTopNames.Add(string.Format("{0} {1}", i, rankMatrix[decadeIndex, i - 1]));
             }
         }
     }
